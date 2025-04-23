c873037d369d257953995f3c1ce87b0fa57b6ff7
HugoMingorance <hugomingorancev@gmail.com>
2025-04-23 12:53:21 +0200

    .

--- AsteraX_UCP_C2_Challenge04_Solution/Assets/__Scripts/LevelAdvancePanel.cs
+++ AsteraX_UCP_C2_Challenge04_Solution/Assets/__Scripts/LevelAdvancePanel.cs
@@ -1,16 +1,37 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelAdvancePanel : MonoBehaviour
 {
     public GameObject playerShip;
+    public int level = 1;
+    public TextMeshProUGUI levelText;
+    private GameObject[] bullets;
+    public Button nextButton;
     
     private void Awake()
     {
         Debug.Log("LevelAdvancePanel Awake");
+        
+        //Level managment
+        level++;
+        levelText.text = "LEVEL " + level.ToString();
+        
+        //Desactivem player
         playerShip.SetActive(false);
+        
+        //Desactivem bullets
+        bullets = GameObject.FindGameObjectsWithTag("Bullet");
+        foreach (GameObject bullet in bullets)
+        {
+            bullet.SetActive(false);
+        }
+        
+        nextButton.onClick.AddListener(OnNextButonClick);
     }
 
     // Start is called before the first frame update
@@ -24,4 +45,13 @@ public class LevelAdvancePanel : MonoBehaviour
     {
         
     }
+
+    public void OnNextButonClick()
+    {
+        AsteraX.GAME_STATE = AsteraX.eGameState.level;
+        playerShip.SetActive(true);
+        gameObject.SetActive(false);
+    }
+    
+    
 }
